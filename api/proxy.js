128e3d07456c0ba24f4c37f8646c0a1c1278f2c2
josuavndev <josuavnsia@gmail.com>
2026-09-01 18:54:40 +0700

    Create proxy.js

---
+++ api/proxy.js
@@ -0,0 +1,30 @@
+const GAS_URL = 'https://script.google.com/macros/s/AKfycbzH4JF301T4z4uZjWNk7PM1OBaJbJ1txds3byT3VtfZe0N5R0o1BhXT_EHFug13p3uF/exec';
+
+export default async function handler(req, res) {
+  try {
+    if (req.method !== 'POST') {
+      return res.status(405).json({
+        status: 'error',
+        message: 'Method not allowed'
+      });
+    }
+
+    const response = await fetch(GAS_URL, {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify(req.body)
+    });
+
+    const data = await response.json();
+
+    return res.status(response.status).json(data);
+
+  } catch (error) {
+    return res.status(500).json({
+      status: 'error',
+      message: error.message
+    });
+  }
+}
