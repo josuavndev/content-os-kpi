b67c9cf1478d1d7e18c962e3a67cb54f5c5b3e27
josuavndev <josuavnsia@gmail.com>
2026-09-05 13:59:39 +0700

    fix: inject selected month into calendar middleware

--- middleware.js
+++ middleware.js
@@ -27,7 +27,7 @@ export default async function middleware() {
 
     // Pass the selected month into the calendar so changing the dropdown re-renders immediately.
     html = html.replace(
-      /(<ClayContentCalendar\\s+contentList=\\{contentList\\})/,
+      /(<ClayContentCalendar\s+contentList=\{contentList\})/,
       '$1\n                  filterMonth={filterMonth}'
     );
 
